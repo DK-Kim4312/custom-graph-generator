3dc138969447d850b4d64fc42eed0187ec2a0b61
DK Kim <dkim693@wisc.edu>
2023-12-10 02:44:31 -0600

    added description and title

--- app/page.js
+++ app/page.js
@@ -4,8 +4,11 @@ import SaveableGraph from '../components/SaveableGraph';
 export default function Home() {
   return (
     <main className={styles.main}>
+      <h1 className={styles.title}>
+        Custom Graph Image Generator
+      </h1>
       <div className={styles.description}>
-        
+        This is a simple graph generator that allows you to add data points and save the graph as a PNG.
       </div>
       <SaveableGraph />
     </main>
